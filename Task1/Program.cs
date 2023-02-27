ee3d92bf558ffc6af542e5278d6dbed60766c342
Zhibek Tadzhibaeva <jibektad@mail.ru>
2023-02-27 17:50:51 +0600

    Home work_1 C#

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Input first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine("Max is " + number1 + "  Min is " + number2);
+}else
+{
+    Console.WriteLine("Max is " + number2 + "  Min is " + number1);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Input numbers");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+if (numbers > 1)
+{
+    while(evenNumber <= numbers)
+    {
+        Console.WriteLine(evenNumber + "   ");
+        evenNumber = evenNumber + 2;
+    }
+}
